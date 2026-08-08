3c21c45cbb563d8b18334711e21fa742b2199c71
curpish <ravenoftheredsky@gmail.com>
2026-08-08 14:45:23 -0400

    ch2l1 main args and stdout messages

--- main.py
+++ main.py
@@ -1,6 +1,17 @@
-def main():
-    print("Hello from web-crawler-in-python!")
+import sys
 
+def main():
+    if len(sys.argv) < 2:
+        print("no website provided")
+        sys.exit(1)
+    if len(sys.argv) > 2:
+        print("too many arguments provided")
+        sys.exit(1)
+    print("Hello from web-crawler-in-python!")
+    print("Script name:", sys.argv[0])  # example.py
+    print("Argument:", sys.argv[1])  # -v
+    base_url = sys.argv[1]
+    print(f"starting crawl of: {base_url}")
 
 if __name__ == "__main__":
     main()
